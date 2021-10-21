3eb346c48f9cb7a563833a8b0f10f788ad810274
ianidi <51965970+ianidi@users.noreply.github.com>
2021-10-21 10:35:01 +0300

    update

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-// Change the second argument to your options:
-// https://github.com/sampotts/plyr/#options
-const player = new Plyr('video', { captions: { active: true } });
+const player = new Plyr('#player');
+
+player.source = {
+    type: 'video',
+    sources: [{
+            src: 'https://d3afmojm4b7xq7.cloudfront.net/daed48a0-139e-42be-a4ea-ad54f45fc983/FileGroup1/BigBuckBunny_480.mp4',
+            type: 'video/mp4',
+            size: 480,
+        },
+        {
+            src: 'https://d3afmojm4b7xq7.cloudfront.net/daed48a0-139e-42be-a4ea-ad54f45fc983/FileGroup1/BigBuckBunny_720.mp4',
+            type: 'video/mp4',
+            size: 720,
+        },
+        {
+            src: 'https://d3afmojm4b7xq7.cloudfront.net/daed48a0-139e-42be-a4ea-ad54f45fc983/FileGroup1/BigBuckBunny_1080.mp4',
+            type: 'video/mp4',
+            size: 1080,
+        },
+    ],
+    poster: 'thumb.jpg',
+};
 
 // Expose player so it can be used from the console
 window.player = player;
