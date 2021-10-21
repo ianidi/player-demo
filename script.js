0468bda970a1f956d54667757e2c8418057363ae
ianidi <51965970+ianidi@users.noreply.github.com>
2021-10-21 15:25:43 +0300

    update

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-const player = new Plyr('#player');
+function initPlayer(elem, instance) {
+    let res;
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open("GET", elem.dataset.source, true);
+    httpRequest.send();
+    httpRequest.addEventListener("readystatechange", function() {
+        if (this.readyState === this.DONE) {
+            res = JSON.parse(this.response);
+            instance.source = res;
+        }
+    });
 
-player.source = {
-    type: 'video',
-    sources: [{
-            src: 'https://d3afmojm4b7xq7.cloudfront.net/daed48a0-139e-42be-a4ea-ad54f45fc983/FileGroup1/BigBuckBunny_480.mp4',
-            type: 'video/mp4',
-            size: 480,
-        },
-        {
-            src: 'https://d3afmojm4b7xq7.cloudfront.net/daed48a0-139e-42be-a4ea-ad54f45fc983/FileGroup1/BigBuckBunny_720.mp4',
-            type: 'video/mp4',
-            size: 720,
-        },
-        {
-            src: 'https://d3afmojm4b7xq7.cloudfront.net/daed48a0-139e-42be-a4ea-ad54f45fc983/FileGroup1/BigBuckBunny_1080.mp4',
-            type: 'video/mp4',
-            size: 1080,
-        },
-    ],
-    poster: 'thumb.jpg',
-};
+}
 
-// Expose player so it can be used from the console
-window.player = player;
+const players = Array.from(document.querySelectorAll('.js-player')).map(elem => {
+    const player = new Plyr(elem);
+    initPlayer(elem, player);
+});
