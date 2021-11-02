33376e9c4d9b4ef647835b03df9d53b719d92f63
ianidi <51965970+ianidi@users.noreply.github.com>
2021-11-02 13:46:54 +0300

    update

--- player.js
+++ player.js
@@ -1,18 +1,5 @@
-function initPlayer(elem, instance) {
-    let res;
-    let httpRequest = new XMLHttpRequest();
-    httpRequest.open("GET", elem.dataset.source, true);
-    httpRequest.send();
-    httpRequest.addEventListener("readystatechange", function() {
-        if (this.readyState === this.DONE) {
-            res = JSON.parse(this.response);
-            instance.source = res;
-        }
-    });
-
-}
-
-const players = Array.from(document.querySelectorAll('.js-player')).map(elem => {
+const players = Array.from(document.querySelectorAll(".js-player")).map(
+  (elem) => {
     const player = new Plyr(elem);
-    initPlayer(elem, player);
-});
+  }
+);
